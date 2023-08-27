4d16295dc94b7aa4cc41f16e4d358d803f90f0db
RicJim <ricardojimenez00759@gmail.com>
2023-08-27 14:23:51 -0500

    Update commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, jsonify, redirect
+from flask import Flask, render_template, request, url_for, jsonify
 import os
 
 app = Flask(__name__)
